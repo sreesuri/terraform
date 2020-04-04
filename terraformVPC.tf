ee0e8484dac56c79290e1cc7f27e36c7656a7415
sreesuri <vunnam118@gmail.com>
2020-04-04 14:12:53 -0700

    add tags

--- terraformVPC.tf
+++ terraformVPC.tf
@@ -1,4 +1,4 @@
-# t2.micro node with an AWS Tag naming it "HelloWorld"
+#To specify AWS region
 provider "aws" {
   region = "us-east-1"
 }
@@ -7,6 +7,9 @@ provider "aws" {
 resource "aws_vpc" "svunnamVPC" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
+  tags {
+    Name = "svunnamVPC"
+    }
 }
 #subnets
 resource "aws_subnet" "svunnam-public-1" {
@@ -14,6 +17,9 @@ resource "aws_subnet" "svunnam-public-1" {
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1a"
+  tags {
+    Name = "svunnam-public-1"
+    }
 }
 
 #subnets
@@ -22,6 +28,9 @@ resource "aws_subnet" "svunnam-public-2" {
   cidr_block = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
+  tags {
+    Name = "svunnam-public-2"
+    }
 }
 
 #subnets
@@ -30,6 +39,9 @@ resource "aws_subnet" "svunnam-private-1" {
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
   availability_zone = "us-east-1a"
+  tags {
+    Name = "svunnam-private-1"
+    }
 }
 
 #subnets
@@ -38,11 +50,17 @@ resource "aws_subnet" "svunnam-private-2" {
   cidr_block = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone = "us-east-1b"
+  tags {
+    Name = "svunnam-private-2"
+    }
 }
 
 #internet Gateway
 resource "aws_internet_gateway" "svunnamIGW" {
   vpc_id = "${aws_vpc.svunnamVPC.id}"
+  tags {
+    Name = "svunnamIGW"
+    }
 }
   
 output "vpc_id" {
