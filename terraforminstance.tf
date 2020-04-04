b07fc0a8c232e34fd57ddb2467e68d4149209db6
sreesuri <vunnam118@gmail.com>
2020-04-03 20:46:16 -0700

    Add code to create VPC

--- terraforminstance.tf
+++ terraforminstance.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "terrafromInstance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+# Create a VPC
+resource "aws_vpc" "svunnamVPC" {
+  cidr_block = "10.0.0.0/16"
+}
