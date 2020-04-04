f40275f521b1f998444703b220e9a024881a80a6
sreesuri <vunnam118@gmail.com>
2020-04-04 13:10:40 -0700

    Create VPC and Subnets using terraform

---
+++ terraformVPC.tf
@@ -0,0 +1,59 @@
+#Internet VPC
+resource "aws_vpc" "svunnamVPC" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags {
+    Name = "svunnamVPC"
+  }
+
+#subnets
+resource "aws_subnet" "svunnam-public-1" {
+  vpc_id = "${aws_vpc.svunnamVPC.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "svunnam-public-1"
+  }
+}
+
+#subnets
+resource "aws_subnet" "svunnam-public-2" {
+  vpc_id = "${aws_vpc.svunnamVPC.id}"
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags {
+    Name = "svunnam-public-2"
+  }
+}
+
+#subnets
+resource "aws_subnet" "svunnam-private-1" {
+  vpc_id = "${aws_vpc.svunnamVPC.id}"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "svunnam-private-1"
+  }
+}
+
+#subnets
+resource "aws_subnet" "svunnam-private-2" {
+  vpc_id = "${aws_vpc.svunnamVPC.id}"
+  cidr_block = "10.0.4.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "us-east-1b"
+  tags{
+    Name = "svunnam-private-2"
+  }
+}
+
+#internet Gateway
+resource "aws_internet_gateway" "svunnamIGW" {
+  vpc_id = "$(aws_vpc.svunnamVPC.id)"
+  tags ={
+    Name = "svunnamVPC"
+  }
+}
