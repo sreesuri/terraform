0c300c36cf7ba6e3e029cf4d5de33efb46d8f178
sreesuri <vunnam118@gmail.com>
2020-04-04 13:47:44 -0700

    Remove Tags

--- terraformVPC.tf
+++ terraformVPC.tf
@@ -2,9 +2,6 @@
 resource "aws_vpc" "svunnamVPC" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
-  tags {
-    Name = "svunnamVPC"
-    }
 }
 #subnets
 resource "aws_subnet" "svunnam-public-1" {
@@ -12,9 +9,6 @@ resource "aws_subnet" "svunnam-public-1" {
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1a"
-  tags {
-    Name = "svunnam-public-1"
-  }
 }
 
 #subnets
@@ -23,9 +17,6 @@ resource "aws_subnet" "svunnam-public-2" {
   cidr_block = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
-  tags {
-    Name = "svunnam-public-2"
-  }
 }
 
 #subnets
@@ -34,9 +25,6 @@ resource "aws_subnet" "svunnam-private-1" {
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
   availability_zone = "us-east-1a"
-  tags {
-    Name = "svunnam-private-1"
-  }
 }
 
 #subnets
@@ -45,17 +33,11 @@ resource "aws_subnet" "svunnam-private-2" {
   cidr_block = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone = "us-east-1b"
-  tags {
-    Name = "svunnam-private-2"
-  }
 }
 
 #internet Gateway
 resource "aws_internet_gateway" "svunnamIGW" {
   vpc_id = "aws_vpc.svunnamVPC.id"
-  tags {
-    Name = "svunnam"
-  }
 }
   
 output "vpc_id" {
