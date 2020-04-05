d1cb5e81d9b60c2e20e36a5da4ebdfdb18b3fc71
sreesuri <vunnam118@gmail.com>
2020-04-05 10:21:53 -0700

    Update terraformVPC.tf

--- terraformVPC.tf
+++ terraformVPC.tf
@@ -62,6 +62,30 @@ resource "aws_internet_gateway" "svunnamIGW" {
     Name = "svunnamIGW"
   }
 }
+
+#Route Tables
+resource "aws_route_table" "svunnam-public" {
+  vpc_id = "${aws_vpc.svunnamVPC.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.svunnamIGW.id}"
+  }
+  tags = {
+    Name = "svunnam-public-1"
+  }
+}
+
+#Route associations public
+resource "aws_route_table_association" "svunnam-public-1-a" {
+  subnet_id  = "${aws_subnet.svunnam-public-1.id}"
+  route_table_id = "${aws_route_table.svunnam-public.id}"
+}
+
+resource "aws_route_table_association" "svunnam-public-2-a" {
+  subnet_id  = "${aws_subnet.svunnam-public-2.id}"
+  route_table_id = "${aws_route_table.svunnam-public.id}"
+}
+
   
 output "vpc_id" {
   description = "The ID of the VPC"
